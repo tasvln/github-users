20ebaaaaa24109ffa412bfe94763047e20989cdd
Adebayo Temitope <adecancode@gmail.com>
2021-01-20 20:18:35 +0100

    +

--- src/components/Search.js
+++ src/components/Search.js
@@ -17,7 +17,7 @@ export default function Search(props) {
     return(
         <div className="my-8 mx-10">
             <form className="flex items-center" onSubmit={handleSubmit}>
-                <input value={username} onChange={event => setUsername(event.target.value)} type='text' className="border border-gray-200 py-2 px-4 rounded-l-xl outline-none text-sm" style={{ height: '40px' }} placeholder='Search...' />
+                <input value={username} onChange={event => setUsername(event.target.value)} type='text' className="border bg-white resize-none border-gray-200 py-2 px-4 rounded-l-xl outline-none text-sm" style={{ height: '40px' }} placeholder='Search...' />
                 <button type="submit" onClick={handleSubmit} className="py-2 px-4 rounded-r-xl bg-gray-900 text-sm border outline-none border-gray-200 -ml-2 text-white font-bold" style={{ height: '40px' }}>Submit</button>
             </form>
         </div>
